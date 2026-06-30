644376ac313846f0806b3951bb40ab3a2decaa96
Mitko <mitkoiliev505@gmail.com>
2026-06-30 18:28:26 +0200

    price update

--- src/pages/menu/drinks/all-drinks.tsx
+++ src/pages/menu/drinks/all-drinks.tsx
@@ -3,19 +3,18 @@ import { useLanguage } from '../../../contexts/language-context';
 import { useMenuPrice } from '../../../hooks/use-menu-price';
 
 const allDrinkItems = [
-  { labelKey: 'all_drinks.coffee', den: 90, euro: 1.5 },
+  { labelKey: 'all_drinks.coffee', den: 100, euro: 1.7 },
   { labelKey: 'all_drinks.water_small', den: 40, euro: 0.7 },
-  { labelKey: 'all_drinks.water_big', den: 60, euro: 1 },
   { labelKey: 'all_drinks.mineral_water_small', den: 50, euro: 0.9 },
   { labelKey: 'all_drinks.dobra_voda', den: 60, euro: 1 },
-  { labelKey: 'all_drinks.cedevita', den: 60, euro: 1 },
+  { labelKey: 'all_drinks.cedevita', den: 70, euro: 1.2 },
   { labelKey: 'all_drinks.coca_cola', den: 70, euro: 1.2 },
   { labelKey: 'all_drinks.fanta', den: 70, euro: 1.2 },
   { labelKey: 'all_drinks.sprite', den: 70, euro: 1.2 },
   { labelKey: 'all_drinks.fresh_orange', den: 120, euro: 2 },
   { labelKey: 'all_drinks.skopsko', den: 100, euro: 1.7 },
   { labelKey: 'all_drinks.smooth', den: 100, euro: 1.7 },
-  { labelKey: 'all_drinks.heineken', den: 150, euro: 2.5 },
+  { labelKey: 'all_drinks.heineken', den: 120, euro: 2 },
 ];
 
 const AllDrinks = () => {
